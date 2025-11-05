ab8b11db7e5d7b6f39344e3d5ce73550a6572d45
Emrik Rellmar <emrik.rellmar@gmail.com>
2025-11-05 10:55:37 +0100

    feat(marketing): add PWA manifest and optional ThirdParty integrations (GA4, GTM, Meta Pixel, LinkedIn, Hotjar, Crisp)

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from 'react-router-dom';
 import Header from './Navigation/Header';
 import Footer from './Navigation/Footer';
+import ThirdParty from './ThirdParty';
 
 function Layout() {
   return (
     <div className="min-h-screen bg-neutral-75">
       <Header />
+      <ThirdParty />
       <main className="mx-auto w-full max-w-7xl px-4 pb-24 pt-28 sm:px-5 lg:px-6 xl:px-8">
         <Outlet />
       </main>
