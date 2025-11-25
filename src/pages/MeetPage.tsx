5d92043afa9bbc8b6c4329b44ae6e01f7d01f853
Emrik Rellmar <emrik.rellmar@gmail.com>
2025-11-25 14:10:36 +0100

    Update

--- src/pages/MeetPage.tsx
+++ src/pages/MeetPage.tsx
@@ -4,12 +4,13 @@ const MeetPage: React.FC = () => {
   return (
     <div className="w-full h-[calc(100vh-80px)] flex flex-col items-center justify-center bg-white">
       <iframe 
-        src="https://calendar.app.google/vdUXfpaFFTtPX6vg6" 
+        src="https://calendar.google.com/calendar/appointments/schedules/AcZssZ2rcxusdOhDjRi4a6g9RSNTz-rYLV3SCR2INtYgy6T9jeUbMAUerT-Ry_OXOj9Q23zZiJmfUZdk?gv=true" 
         style={{ border: 0 }} 
         width="100%" 
         height="100%" 
         frameBorder="0"
         title="Schedule a meeting"
+        allow="camera; microphone; fullscreen"
       ></iframe>
     </div>
   );
