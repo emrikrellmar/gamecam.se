d9dbbdd114a4623224a0807bcc8fa1d8acbc8cf4
Emrik Rellmar <emrik.rellmar@gmail.com>
2025-12-22 13:48:56 +0100

    fix: updated aspect ratio on youtube video embeed

--- src/pages/VideoPage.tsx
+++ src/pages/VideoPage.tsx
@@ -2,16 +2,18 @@ import React from 'react';
 
 const VideoPage: React.FC = () => {
   return (
-    <div className="w-full h-[calc(100vh-80px)] flex flex-col items-center justify-center bg-white">
-      <iframe
-        src="https://www.youtube.com/embed/IzHm5CCqLXk"
-        style={{ border: 0 }}
-        width="100%"
-        height="100%"
-        title="GAMETRAQ Demo Video"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen"
-        allowFullScreen
-      ></iframe>
+    <div className="w-full h-[calc(100vh-80px)] flex flex-col items-center justify-center bg-white px-4">
+      <h1 className="text-2xl font-bold mb-6 text-center">GAMETRAQ demo video</h1>
+      <div className="w-full max-w-3xl aspect-video bg-black rounded-lg overflow-hidden shadow-lg">
+        <iframe
+          src="https://www.youtube.com/embed/IzHm5CCqLXk"
+          style={{ border: 0 }}
+          className="w-full h-full"
+          title="GAMETRAQ Demo Video"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen"
+          allowFullScreen
+        ></iframe>
+      </div>
     </div>
   );
 };
